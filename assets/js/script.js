79f2f8fdc1d3c1b54a878577cc0df96d822711d5
Chiane165 <chianekasselborgonia@gmail.com>
2025-05-15 00:54:01 +0800

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+let currentSlideEducation = 0;
+let currentSlideProjects = 0;
+const slideWidth = 600;
+
+// Function to move to the next or previous slide for Education
+function moveSlideEducation(direction) {
+    const slides = document.querySelector(".educationslides");
+    const totalSlides = slides.children.length;
+
+    currentSlideEducation += direction;
+
+    if (currentSlideEducation < 0) {
+        currentSlideEducation = totalSlides - 1; 
+    }
+    else if (currentSlideEducation >= totalSlides) {
+        currentSlideEducation = 0;
+    }
+
+    slides.style.transform = `translateX(-${currentSlideEducation * slideWidth}px)`;
+}
+
+function moveSlideProjects(direction) {
+    const slides = document.querySelector(".projectsslides");
+    const totalSlides = slides.children.length;
+
+    currentSlideProjects += direction;
+
+    if (currentSlideProjects < 0) {
+        currentSlideProjects = totalSlides - 1;
+    }
+    else if (currentSlideProjects >= totalSlides) {
+        currentSlideProjects = 0;
+    }
+
+    slides.style.transform = `translateX(-${currentSlideProjects * slideWidth}px)`;
+}
+
+const contactWindow = document.getElementById('contactWindow');
+const openContactWindowBtn = document.getElementById('openContactWindowBtn');
+const closeContactWindowBtn = document.getElementsByClassName('close-contact')[0];
+
+
+openContactWindowBtn.onclick = function() {
+    contactWindow.style.display = "block";
+    setTimeout(() => contactWindow.style.transform = "translateY(0)", 10);
+}
+
+closeContactWindowBtn.onclick = function() {
+    contactWindow.style.transform = "translateY(100%)";
+    setTimeout(() => contactWindow.style.display = "none", 300);
+}
+
+window.onclick = function(event) {
+    if (event.target === contactWindow) {
+        contactWindow.style.transform = "translateY(100%)";
+        setTimeout(() => contactWindow.style.display = "none", 300);
+    }
+}
